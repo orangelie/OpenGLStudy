96a8565abcb294b9c0e1e6c17cc1c2e2b9c8dd56
Mawi1e <sw031489@naver.com>
2022-04-03 02:43:24 +0900

    OpenGLStudy

---
+++ 2. Hello Triangle (2022-04-02~2022-04-03)/ReadMe.md
@@ -0,0 +1,28 @@
+![header](https://capsule-render.vercel.app/api?text=Mawi1e!&fontSize=50&rotate=0&color=38303f&fontColor=ff0099&type=Waving&animation=scaleIn)
+
+😀 Hello, everyone.
+This project uses **OpenGL** to record and share what I personally studied.
+🎮 I want to share various and convenient **game development tools** for those who are not good at game development **in their daily lives**. 🎮
+
+## ✏️ Today's study.
+**[ Hello Triangle ]**  
+\- Understand the concept of buffer resources. ✅   
+\- Understand the shader program in opengl. ✅   
+
+
+## 🔑 How to use?
+**[1]**: Convert to Solid(Fill) Mode.
+**[2]**: Convert to Wireframe Mode.
+**[Esc]**: Close a Window.
+
+## 📷 Photo
+![](https://github.com/Mawi1e/OpenGLStudy/blob/main/2.%20Hello%20Triangle%20(2022-04-02~2022-04-03)/pict/triangle.png)
+
+## 🗡️ Skills
+### Platform & Languages
+<img src="https://img.shields.io/badge/C%2B%2B-9a00e6?style=flat-square&logo=C%2B%2B&logoColor=white"/> <img src="https://img.shields.io/badge/Python-ff9533?style=flat-square&logo=PYTHON&logoColor=white"/>  <img src="https://img.shields.io/badge/Lua-ff03ff?style=flat-square&logo=LUA&logoColor=white"/> <img src="https://img.shields.io/badge/Rust-38303f?style=flat-square&logo=RUST&logoColor=white"/>
+### Tools
+<img src="https://img.shields.io/badge/Win32API-38303f?style=flat-square&color=white&logo=MICROSOFT&logoColor=f41e48"/> <img src="https://img.shields.io/badge/DirectX-38303f?style=flat-square&color=white&logo=MICROSOFT&logoColor=02afb7"/>
+
+<br></br>
+***Do not give up.***
---
+++ 2. Hello Triangle (2022-04-02~2022-04-03)/src/HelloTriangle.cpp
@@ -0,0 +1,102 @@
+#include "HelloTriangle.h"
+
+HelloTriangle::HelloTriangle() {
+
+}
+
+HelloTriangle::~HelloTriangle() {
+
+}
+
+void HelloTriangle::init(const char* windowName, int screenWidth, int screenHeight, GLFWkeyfun keyCallbackFunc) {
+	OpenGLApp::init(windowName, screenWidth, screenHeight, keyCallbackFunc);
+
+	const char* vSrv = "#version 330 core\n"
+		"layout(location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\n";
+
+	const char* fSrv = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n";
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vSrv, NULL);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	char infoLog[512] = {};
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return;
+	}
+
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fSrv, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return;
+	}
+
+	program = glCreateProgram();
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+
+	glGetShaderiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(program, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		return;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// ===============================================================================
+	float vertices[] = {
+		0.5f,  0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f,
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (const void*)0);
+	glEnableVertexAttribArray(0);
+}
+
+void HelloTriangle::draw() {
+	glUseProgram(program);
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+}
